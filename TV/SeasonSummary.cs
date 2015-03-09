e3d14188471acf771326b25e7c7a18874e79c548
Fishes <a.snoek@xs4ll.nl>
2015-03-09 19:43:42 +0100

    Added missing file

---
+++ TV/SeasonSummary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TmdbWrapper.Utilities;
+
+namespace TmdbWrapper.TV
+{
+    /// <summary>
+    /// A season summary of a TV Series
+    /// </summary>
+    public class SeasonSummary : ITmdbObject
+    {
+        /// <summary>
+        /// The air date of the the season
+        /// </summary>
+        public DateTime? AirDate { get; private set; }
+        /// <summary>
+        /// The id of the season
+        /// </summary>
+        public int Id { get; private set; }
+        /// <summary>
+        /// The path to the poster of the season
+        /// </summary>
+        public string PosterPath { get; private set; }
+        /// <summary>
+        /// The sequence number of the season
+        /// </summary>
+        public int SeasonNumber { get; private set; }
+
+        void ITmdbObject.ProcessJson(JSONObject jsonObject)
+        {
+            AirDate = jsonObject.GetSafeDateTime("air_date");
+            Id = (int)jsonObject.GetSafeNumber("id");
+            PosterPath = jsonObject.GetSafeString("poster_path");
+            SeasonNumber = (int)jsonObject.GetSafeNumber("season_number");
+        }
+
+        #region Image Uri's
+        /// <summary>
+        /// Uri to the poster image.
+        /// </summary>
+        /// <param name="size">The size for the image as required</param>
+        /// <returns>The uri to the sized image</returns>
+        public Uri Uri(PosterSize size)
+        {
+            return Utilities.Extensions.MakeImageUri(size.ToString(), PosterPath);
+        }
+        #endregion
+    }
+}
